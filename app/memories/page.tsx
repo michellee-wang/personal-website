9f76e52d86ad01c90f1ab247a62f1e2b521329d2
michellee-wang <michellewang857@gmail.com>
2025-04-08 22:39:34 -0400

    removed x link

--- app/memories/page.tsx
+++ app/memories/page.tsx
@@ -38,11 +38,6 @@ export default function Memories() {
               <circle cx="4" cy="4" r="2"></circle>
             </svg>
           </a>
-          <a href="https://x.com/michellewang857" target="_blank" rel="noopener noreferrer">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#D3ABAB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
-            </svg>
-          </a>
         </div>
         
         {/* Photo Grid - Mobile grid and desktop varied layout */}
@@ -613,11 +608,6 @@ export default function Memories() {
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
               <rect x="2" y="9" width="4" height="12"></rect>
               <circle cx="4" cy="4" r="2"></circle>
-            </svg>
-          </a>
-          <a href="https://x.com/michellewang857" target="_blank" rel="noopener noreferrer">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#D3ABAB" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
             </svg>
           </a>
         </div>
